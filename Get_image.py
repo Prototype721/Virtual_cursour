c6ff74a4658bed7da51083ebde28da780aa34f28
Mikhail Golikov <prototpe7212@gmail.com>
2024-09-15 16:21:40 +0300

    Get image

---
+++ Get_image.py
@@ -0,0 +1,28 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Error: Camera could not be opened.")
+    exit()
+
+def show():
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Error: Frame could not be captured.")
+        end_showing()
+        return 1
+
+    # Display the resulting frame
+    cv2.imshow('Camera Feed', frame)
+
+    # Press 'q' on the keyboard to exit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        end_showing()
+        return 2
+    return 0
+
+def end_showing():
+    cap.release()
+    cv2.destroyAllWindows()
---
+++ main.py
@@ -0,0 +1,9 @@
+import Get_image
+
+
+if __name__ == '__main__':
+    while True:
+        flag = Get_image.show()
+
+        if flag > 0:
+            break
