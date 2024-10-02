1a5216392723d768302ed84f395702e4393847a4
Mikhail Golikov <prototpe7212@gmail.com>
2024-10-02 18:27:46 +0300

    Add dirty work for check

--- Yolo_dirty_work.py
+++ Yolo_dirty_work.py
@@ -3,14 +3,22 @@ import cv2
 import numpy as np
 import math
 
+
+# Load the YOLO model
 model = YOLO('Models/yolov8s.pt')
+
+
 cap = cv2.VideoCapture(0)
 
 while True:
     success, img = cap.read()
     if not success:
-        print('No sucsess')
+        print('Cant read image')
         break
+    
+    # Создаем черный экран той же формы, что и изображение
+    mask = np.zeros_like(img)
+
 
     results = model(img, stream = True)
     detections = np.empty((0, 5))
@@ -23,15 +31,16 @@ while True:
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
 
             conf = math.ceil((box.conf[0] * 100)) / 100 # confidence
-            cls = int(box.cls[0]) # class id
 
             if conf > 0.3:
-                cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3)
-                cv2.putText(img, f'{conf} - {cls}',
-                    (max(5, x1), max(15, y1-10)), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (100, 0, 255), 2)
-                
+                cv2.rectangle(mask, (x1, y1), (x2, y2), (255, 255, 255), -1)
+    
+
+    # Применяем побитовую операцию AND
+    img_strip = cv2.bitwise_and(img, mask)            
 
     cv2.imshow('Camera Feed', img)
+    cv2.imshow('Strip image', img_strip)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cap.release()
